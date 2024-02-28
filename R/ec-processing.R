f1ec6216bbfb89dd3278afc84c4fc5e0dbbcc166
noctiluc3nt <laura.mack@fu-berlin.de>
2024-02-28 19:41:50 +0100

    quality flagging

---
+++ R/ec-processing.R
@@ -0,0 +1,150 @@
+#' Double rotation
+#'
+#'@description Double rotation
+#'@param u u-wind (levelled sonic)
+#'@param v v-wind (levelled sonic)
+#'@param w w-wind (levelled sonic)
+#'
+#'@return list containing the wind in a natural coordinate system (streamwise, crosswise, vertical) and the two rotation angles theta and phi
+#'@export
+#'
+#'@examples
+#'
+#'
+rotate_double = function(u,v,w) {
+	#horizontal
+	theta=atan2(mean(v),mean(u))
+	rot1=matrix(c(cos(theta),-sin(theta),0,sin(theta),cos(theta),0,0,0,1), nrow=3,ncol=3,byrow=TRUE) #B bzw. M^T
+	wind1=c(u,v,w)%*%rot1
+	#vertical
+	phi=atan2(mean(w),mean(u))
+	rot2=matrix(c(cos(phi),0,-sin(phi),0,1,0,sin(phi),0,cos(phi)),nrow=3,ncol=3,byrow=TRUE) #D^T
+	wind2=wind1%*%rot2
+	return(list("wind"=wind2,"theta"=theta*180/pi,"phi"=phi*180/pi))
+}
+
+
+#' Planar fit rotation
+#'
+#'@description Planar fit rotation
+#'@param u u-wind (levelled sonic)
+#'@param v v-wind (levelled sonic)
+#'@param w w-wind (levelled sonic)
+#'
+#'@return 
+#'@export
+#'
+#'@examples
+#'
+#'
+rotate_planar = function(u,v,w) {
+	#TODO
+	theta=atan2(mean(v),mean(u))
+	rot1=matrix(c(cos(theta),-sin(theta),0,sin(theta),cos(theta),0,0,0,1), nrow=3,ncol=3,byrow=TRUE) #B bzw. M^T
+	wind1=c(u,v,w)%*%rot1
+	return(list("wind"=wind1,"theta"=theta*180/pi)
+}
+
+
+#' Stationarity Flag
+#'
+#'@description Stationarity Flag according to Foken and Wichura, 1996 based on the assumption that the covariance of two variables (var1 and var2, one usually representing vertical velocity) calculated for blocks (of length nsub) do not differ to much from the total covariance
+#'@param var1 variable 1 
+#'@param var2 variable 2 (same length as var1, usually either var1 or var2 represent vertical velocity)
+#'@param nsub number of elements used for subsampling (nsub < length(var1)) 
+#'
+#'@return 
+#'@export
+#'
+#'@examples
+#'set.seed(5)
+#'ts1=rnorm(30)
+#'ts2=rnorm(30)
+#'flag_stationarity(var1,var2)
+#'
+flag_stationarity = function(var1,var2,nsub=6) {
+    if (length(var1) != length(var2)) {
+        print("ERROR: var1 and var2 have to be of equal length.")
+    }
+    nint=length(var1)%/%nsub
+    if (nint<=1) {
+        print("WARNING: nsub is chosen to large.")
+    }
+    rnfs=array(NA,dim=nint)
+    cov_complete=cov(var1,var2)
+    for (i in 1:nint) {
+        isub=((i-1)*nsub+1):(i*nsub)
+        cov_sub=cov(var1[isub],var2[isub])
+        rnfs[i]=abs((cov_complete-cov_sub)/cov_complete)
+    }
+    rnf=mean(rnfs,na.rm=T)
+    flag=ifelse(rnf<0.3,0,ifelse(rnf<1,1,2))
+    return(flag)
+}
+
+
+#' Vertical Velocity Flag
+#'
+#'@description Vertical Velocity Flag according to Mauder et al., 2013: After (planar fit) rotation the vertical velocity should vanish, this flag flags high remaining vertical velocities.
+#'@param w vertical velocity 
+#'
+#'@return 
+#'@export
+#'
+#'@examples
+#'
+flag_w = function(w) {
+    w=abs(w)
+    flag=ifelse(w<0.1,0,ifelse(1<0.15,1,2))
+    return(flag)
+}
+
+#' Flow Distortion Flag and Wind Constancy Ratio
+#'
+#'@description Flow Distortion Flag according to Mauder et al., 2013: Wind coming from a (predefined) direction blocked by the measurement device is flaged with 2 (for wind speeds greater than 0.1 assuming that during calm wind the wind direction is not well-defined). The wind constancy ratio is calculated to quantify the variability of horizontal wind direction according to Mahrt, 1999.
+#'@param u u-wind (levelled sonic)
+#'@param v v-wind (levelled sonic)
+#'@param dir_blocked vector containing the lower and upper bound of the blocked wind sector in degrees (e.g., dir_blocked=c(30,60))
+#'@param threshold_cr threshold for constancy ratio (default threshold_cr=0.9, may be adapted to used data set)
+#'
+#'@return 
+#'@export
+#'
+#'@examples
+#'
+flag_distortion = function(u,v,dir_blocked=c(30,60),threshold_cr=0.9) {
+    if (length(u) != length(v)) {
+        print("ERROR: u and v have to be of equal length.")
+    }
+    #horizontal wind speed
+    ws=sqrt(mean(u)^2+mean(v)^2)
+    #constancy ratio cr
+    cr=sqrt(sum(u^2)+sum(v^2))/(ws)
+    #flow distortion flag considering cr
+    if (ws>0.1 & cr>threshold_cr) {
+        wd=atan2(mean(v),mean(u))
+        flag=ifelse(wd>=dir_blocked[1] & wd<=dir_blocked[2],2,0)
+    } else {
+        flag=0
+    }
+    return(flag)
+}
+
+#' Integral Turbulence Characteristics Flag 
+#'
+#'@description Integral Turbulence Characteristics Flag: Tests the consistency with Monin-Obukhov similarity theory using the scaling functions from Panofsky and Dutton, 1984
+#'@param sigma_w standard deviation of vertical velocity
+#'@param ustar friction velocity
+#'@param zeta stability parameter zeta = z/L
+#'
+#'@return 
+#'@export
+#'
+#'@examples
+#'
+flag_most = function(sigma_w,ustar) {
+    parameterized=1.3*(1+2*abs(zeta))^(1/3) #sigma_w/ustar parametrized according to scaling function based on zeta
+    itc=abs((sigma_w/ustar-parameterized)/parameterized)
+    flag=ifelse(itc<0.3,0,ifelse(itc<0.8,1,2))
+    return(flag)
+}
