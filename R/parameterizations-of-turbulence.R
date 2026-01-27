cdd448bdd24d0e4caf876b6e433b82f48b35b50c
noctiluc3nt <laura.mack@fu-berlin.de>
2026-01-27 23:19:04 +0100

    stability correction functions for stable conditions, diffusivities, T2m diagnostic (FM, FH, CM, CH, T2m)

--- R/parameterizations-of-turbulence.R
+++ R/parameterizations-of-turbulence.R
@@ -189,6 +189,144 @@ calc_phix = function(x_sd,x,ustar) {
 	return(x_sd/xstar)
 }
 
+#' Stability correction function for momentum (FM), used for calculating momentum diffusivity
+#'
+#'@description stability correction function FM -- so far for stable conditions only (Ri > 0)
+#'@param Ri bulk Richardson number [-]
+#'@param method defining from which paper the stability correction function should be used, default \code{method="LTG"} for the ones used in HARMONIE-AROME, other options: \code{method="L1979"} for Louis, 1979, \code{method="LTGrevised"} for Viterbo et al, 1999, \code{method="UKMO"} for UK MetOffice
+#'
+#'@return FM
+#'@export
+#'
+#'@examples
+#'calc_FM(0.1)
+#'calc_FM(0.1,method="L1979")
+#'calc_FM(-0.1,method="UKMO")
+#'
+calc_FM = function(Ri,method="LTG") {
+    if (method=="L1979" | method == "Louis1979") { 
+        #Louis, 1979
+        #return(ifelse(Ri<0,,1/(1+4.7*Ri)^2))
+        return(1/(1+4.7*Ri)^2)
+    } else if (method == "LTG" | method=="LTG1982" | method =="AROME" | method == "arome") {
+        #LTG / HARMONIE-AROME
+        #Ch=75*karman()*sqrt(1+z/z0)/(log(1+z/z0))^2 #in the surface layer
+        #return(ifelse(Ri<0,1-10*Ri/(1+Ch*sqrt(-Ri)),1/(1+10*Ri/(sqrt(1+5*Ri)))))
+        return(1/(1+10*Ri/(sqrt(1+5*Ri))))
+    } else if (method == "LTGrevised") {
+        #LTG revised / Viterbo, 1999 / ECMWF until 2007
+        #return(ifelse(Ri<0,,1/(1+10*Ri/(sqrt(1+Ri)))))
+        return(1/(1+10*Ri/(sqrt(1+Ri))))
+    } else if (method == "UKMO") {
+        #UKMO (from Cuxart et al, 2006)
+        #return(ifelse(Ri<0,,1/(1+10*Ri)))
+        return(1/(1+10*Ri))
+    } else if (method == "G1988" | method == "Geleyn1988" | method == "BD") {
+        #Geleyn, 1988 / Businger-Dyer relation
+        zeta=Ri2zeta(Ri)
+        return(1+5*zeta)
+    } else if (method == "D1991" | method == "Duynkerke1991") {
+        #Duynkerke, 1991 -- uses orignally local stability parameter
+        zeta=Ri2zeta(Ri)
+        return(1+5*zeta*(1+5/0.8*zeta)^(0.2))
+    } else {
+        warning("The chosen method to calculate FM is not available.")
+    }  
+    #BH, 1991
+    #todo
+}
+
+#' Stability correction function for heat (FH), used for calculating heat diffusivity
+#'
+#'@description stability correction function FM -- so far for stable conditions only (Ri > 0)
+#'@param Ri bulk Richardson number [-]
+#'@param method defining from which paper the stability correction function should be used, default \code{method="LTG"} for the ones used in HARMONIE-AROME, other options: \code{method="L1979"} for Louis, 1979, \code{method="LTGrevised"} for Viterbo et al, 1999, \code{method="UKMO"} for UK MetOffice
+#'
+#'@return FH
+#'@export
+#'
+#'@examples
+#'calc_FH(0.1)
+#'calc_FH(0.1,method="L1979")
+#'calc_FH(-0.1,method="UKMO")
+#'
+calc_FH = function(Ri,method="LTG") {
+    if (method=="L1979" | method == "Louis1979") { 
+        #Louis, 1979
+        #return(ifelse(Ri<0,,1/(1+4.7*Ri)^2))
+        return(1/(1+4.7*Ri)^2)
+    } else if (method == "LTG" | method=="LTG1982" | method =="AROME" | method == "arome") {
+        #LTG / HARMONIE-AROME
+        #Ch=75*karman()*sqrt(1+z/z0)/(log(1+z/z0))^2 #in the surface layer
+        #return(ifelse(Ri<0,1-15*Ri/(1+Ch*sqrt(-Ri)),1/(1+15*Ri*(sqrt(1+5*Ri)))))
+        return(1/(1+15*Ri*(sqrt(1+5*Ri))))
+    } else if (method == "LTGrevised") {
+        #LTG revised / Viterbo, 1999 / ECMWF until 2007
+        #return(ifelse(Ri<0,,1/(1+10*Ri*(sqrt(1+Ri)))))
+        return(1/(1+10*Ri*(sqrt(1+Ri))))
+    } else if (method == "UKMO") {
+        #UKMO (from Cuxart et al, 2006)
+        #return(ifelse(Ri<0,,1/(1+10*Ri)))
+        return(1/(1+10*Ri))
+    } else if (method == "G1988" | method == "Geleyn1988" | method == "BD") {
+        #Geleyn, 1988 / Businger-Dyer relation
+        zeta=Ri2zeta(Ri)
+        return(1+5*zeta)
+    #} else if (method == "BH1991") {
+        #Beljaars and Holtslag, 1991
+        #zeta=Ri2zeta(Ri)
+        #return()
+    } else if (method == "D1991" | method == "Duynkerke1991") {
+        #Duynkerke, 1991 -- uses orignally local stability parameter
+        zeta=Ri2zeta(Ri)
+        return(1+5*zeta*(1+5/0.8*zeta)^(0.2))
+    } else {
+        warning("The chosen method to calculate FM is not available.")
+    }  
+    #BH, 1991
+    #todo
+}
+
+
+#' Diffusivity of momentum
+#'
+#'@description calculates momentum diffusivity
+#'@param Ri bulk Richardson number [-]
+#'@param z measurement height [m]
+#'@param z0 roughness length for momentum [m] 
+#'@param method defining from which paper the stability correction function should be used, details see function \code{calc_FM}
+#'
+#'@return CM, momentum diffusivity coefficient
+#'@export
+#'
+#'@examples
+#'calc_CM(0.1)
+#'
+calc_CM = function(Ri,z=10,z0=0.001,method="LTG") {
+    CN=(karman()/log(z/z0)) #diffusivity under neutral conditions
+    return(calc_FM(Ri,method=method)*CN)
+}
+
+#' Diffusivity of heat
+#'
+#'@description calculates heat diffusivity
+#'@param Ri bulk Richardson number [-]
+#'@param z measurement height [m]
+#'@param z0 roughness length for momentum [m] 
+#'@param z0H roughness length for heat [m], default \code{z0H=NULL} for using z0H = z0/10 (as in HARMONIE-AROME)
+#'@param method defining from which paper the stability correction function should be used, details see function \code{calc_FH}
+#'
+#'@return CH, diffusivity coefficient of heat
+#'@export
+#'
+#'@examples
+#'calc_CH(0.1)
+#'
+calc_CH = function(Ri,z=10,z0=0.001,z0H=NULL,method="LTG") {
+    z0=ifelse(!is.null(z0H),z0H,z0/10) #overwrite z0 bei z0H for calculation 
+    CN=(karman()/log(z/z0)) #diffusivity under neutral conditions
+    return(calc_FH(Ri,method=method)*CN)
+}
 
 #' Wind profile from Monin-Obukhov similarity theory
 #'
@@ -220,7 +358,7 @@ calc_windprofile = function(zs,ustar,z0=0,d=0,zeta=0) {
 	return(data.frame("height"=zs,"windspeed"=uz))
 }
 
-#' Temperatyre profile from Monin-Obukhov similarity theory
+#' Temperature profile from Monin-Obukhov similarity theory
 #'
 #'@description Calculates vertical profile of horizontal wind speed following Monin-Obukhov similarity theory
 #'@param zs scalar or vector, heights [m] at which the horizontal wind speed should be calculate 
@@ -251,6 +389,38 @@ calc_tempprofile = function(zs,ustar,z0=0,d=0,zeta=0,T0=273.15) {
 	return(data.frame("height"=zs,"temperature"=Tz))
 }
 
+
+#' Calculates T2m diagnostic from surface temperature and temperature at first model level using the Geleyn, 1988 interpolation (used in HARMONIE-AROME)
+#'
+#'@description Calculates T2m diagnostic from surface temperature and temperature at first model level using the Geleyn, 1988 interpolation (used in HARMONIE-AROME)
+#'@param z height [m] to be interpolated at, for T2m \code{z=2} (2 m)
+#'@param za height of first atmospheric model level [m] (or measurement height when applied to observations)
+#'@param Ta temperature [K] at za
+#'@param Ts surface temperature [K]
+#'@param Ri Richardson number [-]
+#'@param z0 roughness length for momentum [m]
+#'@param z0H roughness length for heat [m], default \code{z0H=NULL} for using z0H = z0/10 (as in HARMONIE-AROME)
+#'@param method method used to calculate stability correction functions FM and FH, default \code{method="LTG"}
+#'
+#'@return interpolated temperature at z (e.g. T2m for z = 2 m)
+#'@export
+#'
+#'@examples
+#'T2m=calc_T2m_diagnostic(2,12,288,285,0.1,0.001)
+#'
+calc_T2m_diagnostic = function(z=2,za=10,Ta,Ts,Ri,z0,z0H=NULL,method="LTG") {
+    if (is.null(z0H)) z0H=z0/10
+    kappa=0.4
+    CN=(kappa/(log((za+z0)/z0)))^2
+    CD=CN*calc_FM(Ri,method=method)
+    CH=CN*calc_FH(Ri,method=method)
+    bN=kappa/sqrt(CN)
+    bH=kappa*sqrt(CD)/CH
+    si=1/bH*(log(1+z/za*(exp(bN)-1)) - z/za*(bN-bH))
+    Tz=Ts+si*(Ta-Ts)
+    return(Tz)
+}
+
 #' Converts stability parameter zeta to Richardson number Ri using Businger-Dyer relations
 #'
 #'@description converts zeta to Ri using Businger-Dyer relations
@@ -264,6 +434,28 @@ calc_tempprofile = function(zs,ustar,z0=0,d=0,zeta=0,T0=273.15) {
 #'
 zeta2Ri = function(zeta) {
     return(0.74*zeta*sqrt(1-15*zeta)/sqrt(1-9*zeta))
+}
+
+#' Converts Richardson number Ri to stability parameter zeta using the relation from Holtslag et al, 2014
+#'
+#'@description converts Ri to zeta using the relation from Holtslag et al, 2014
+#'@param Ri Richardson number [-] (bulk or gradient, see argument \code{gradient_method}) 
+#'@param gradient_method method to calculate vertical gradient, either \code{method="bulk"} for surface to atmosphere or \code{method="gradient"} for two atmospheric levels
+#'
+#'@return stability parameter zeta [-]
+#'@export
+#'
+#'@examples
+#'zeta_transformed=Ri2zeta(0.1)
+#'
+Ri2zeta = function(Ri,gradient_method="bulk") {
+    if (gradient_method == "bulk") {
+        return(ifelse(Ri<0,10*Ri,10*Ri/(1-5*Ri)))
+    } else if (gradient_method == "gradient" | gradient_method == "grad") {
+        return(ifelse(Ri<0,Ri,Ri/(1-5*Ri)))
+    } else {
+        warning("The chosen calculation method for the gradients is not available.")
+    }
 }
 
 
